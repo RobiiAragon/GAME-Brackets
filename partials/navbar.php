e15cfdb04e12ea3da0685b6cf48a25794057dd88
RobiiAragon <robiiaragon@gmail.Com>
2022-11-27 00:16:11 -0800

    cambios

---
+++ partials/navbar.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>CodePen - Menu Hover Line Effect </title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+<link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<nav>
+  <ul class="menu" class="logo">
+  <a href="../FINAL/INDEX.php"> <img src=".static/IMG/logo.png" alt="" width="125" height="70" /> </a>
+  <li><a href="../FINAL/catalogo.php">Juegos</a></li>
+    <li><a href="../FINAL/ranking.php">Ranks</a></li>
+    <li><a href="../FINAL/register.php">Registrate</a></li>
+    <li><a href="../FINAL/login.php">Iniciar Sesion</a></li>
+  </ul>
+</nav>
+<!-- partial -->
+  
+</body>
+</html>
