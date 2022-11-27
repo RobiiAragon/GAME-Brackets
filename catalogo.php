59fcc5ab80a9791b517c3f5ccb79934c93a5bded
RobiiAragon <robiiaragon@gmail.Com>
2022-11-27 15:21:10 -0800

    a

--- catalogo.php
+++ catalogo.php
@@ -5,70 +5,67 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
 <link rel="stylesheet" href="./partials/navbar.css">
 <link rel="stylesheet" href="./partials/header.css">
+<link rel="stylesheet" href="./static/css/catalogo.css">
 
 <header>
-<nav>
+  <nav>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand font-weight-bold" href="./index.php">
-
-
-
-    
-      <ul class="menu">
-      <img class="mr-2" src="./static/img/logo.png" />
-        <li class="nav-item">
-          <a class="nav-link" href="../FINAL/catalogo.php">Juegos</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="../FINAL/ranking.php">leaderboard</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="../FINAL/register.php">Registrate</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="../FINAL/login.php">Iniciar Sesion</a>
-        </li>
-      </ul>
-</nav>
-</nav>
-</head>
-
-
-<head>
-    <title>
-        
-    </title>
-      
-    <style>
-        .sudo {
-            width: 256px;
-            height: 256px;
-            margin: 50px;
-            background-image: url(
-"https://raw.githubusercontent.com/RobiiAragon/Proyecto-esperanza/main/static/img/games/256x/Gray_smash.png");
-        }
-  
-        .sudo:hover {
-            background-image: url(
-"https://raw.githubusercontent.com/RobiiAragon/Proyecto-esperanza/main/static/img/games/256x/smash.png");
-        }
-    </style>
-</head>
-  
-<body>
-    <h2>Smash bros</h2>
-  
-
-  
-    <div class="sudo"></div>
-</body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand font-weight-bold" href="./index.php">
 
 
 
-<body>
-<h2>Sponsors</h2>
-<?php require "./partials/header.php" ?>
-</body>
-<?php require "partials/footer.php" ?>
+
+          <ul class="menu">
+            <img class="mr-2" src="./static/img/logo.png" />
+            <li class="nav-item">
+              <a class="nav-link" href="../FINAL/catalogo.php">Juegos</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../FINAL/ranking.php">leaderboard</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../FINAL/register.php">Registrate</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../FINAL/login.php">Iniciar Sesion</a>
+            </li>
+          </ul>
+    </nav>
+  </nav>
+  </head>
+
+  <!----------------------------------------------Smash----------------------------------------------------->
+
+  <head>
+    <div class="sudo4"></div>
+  </head>
+
+  <!----------------------------------------------Overwatch----------------------------------------------------->
+
+  <head>
+  <div class="sudo3"></div>
+  </head>
+
+  <!----------------------------------------------LOL----------------------------------------------------->
+  <head>
+
+
+  <div class="sudo2"></div>
+  </head>
+
+
+  <!----------------------------------------------CSGO----------------------------------------------------->
+  <head>
+    <div class="sudo1"></div>
+  </head>
+
+
+
+
+  <body>
+    <h2>Sponsors</h2>
+    <?php require "./partials/header.php" ?>
+  </body>
+  <?php require "partials/footer.php" ?>
