94eedfc50dbb9017db59cdb879a5b7a7aa184be7
RobiiAragon <robiiaragon@gmail.Com>
2022-11-28 22:38:34 -0800

    a

--- ranking.php
+++ ranking.php
@@ -1,20 +1,78 @@
 <!DOCTYPE html>
 <html lang="en">
-<meta charset="UTF-8">
-<title>LVPO</title>
-<link rel="stylesheet" href="./partials/navbar.css">
-<link rel="stylesheet" href="./partials/header.css">
 
-<header>
-<nav>
-<?php require "./partials/navbar.php" ?>
-</nav>
+<head>
+    <meta charset="UTF-8">
+    <title>LVPO</title>
+    <link rel="stylesheet" href="./static/css/style.css">
 </head>
 
-
-
-
 <body>
-<?php require "./partials/header.php" ?>
-</body>
-<?php require "partials/footer.php" ?>
+    <!-- partial:index.partial.html -->
+    <nav class="nav">
+        <div class="container">
+            <div class="logo">
+                <a href="./index.php">
+                    <img class="mr-2" src="./static/img/logo.png" />
+            </div>
+            </a>
+
+        </div>
+        <div id="mainListDiv" class="main_list">
+            <ul class="navlinks">
+                <li><a href="../FINAL/catalogo.php">Juegos</a></li>
+                <li><a href="../FINAL/ranking.php">leaderboard</a></li>
+                <li><a href="../FINAL/register.php">Registrate</a></li>
+                <li><a href="../FINAL/login.php">Iniciar Sesion</a></li>
+            </ul>
+        </div>
+        <span class="navTrigger">
+            <i></i>
+            <i></i>
+            <i></i>
+        </span>
+        </div>
+    </nav>
+    </div>
+
+
+    <section class="home">
+    </section>
+    <div style="height: 1000px">
+        <!-- just to make scrolling effect possible -->
+        <h2 class="myH2">Liga de videojuegos profesional online</h2>
+        <p class="myP">This is a responsive fixed navbar animated on scroll</p>
+        <p class="myP">I took inspiration from ABDO STEIF </p>
+        <p class="myP">I HOPE YOU FIND THIS USEFULL</p>
+        <p class="myP">UTT</p>
+
+        <p class="myP">
+
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum ratione facere animi impedit rem labore sint repellendus ipsa sapiente voluptatem aut excepturi quo itaque, ab earum cumque. Voluptatem beatae id inventore quod voluptate qui deserunt, quis placeat, tempora ex totam, dolore sequi harum eos voluptatibus animi labore officiis minus laboriosam
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum ratione facere animi impedit rem labore sint repellendus ipsa sapiente voluptatem aut excepturi quo itaque, ab earum cumque. Voluptatem beatae id inventore quod voluptate qui deserunt, quis placeat, tempora ex totam, dolore sequi harum eos voluptatibus animi labore officiis minus laboriosam
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum ratione facere animi impedit rem labore sint repellendus ipsa sapiente voluptatem aut excepturi quo itaque, ab earum cumque. Voluptatem beatae id inventore quod voluptate qui deserunt, quis placeat, tempora ex totam, dolore sequi harum eos voluptatibus animi labore officiis minus laboriosam
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum ratione facere animi impedit rem labore sint repellendus ipsa sapiente voluptatem aut excepturi quo itaque, ab earum cumque. Voluptatem beatae id inventore quod voluptate qui deserunt, quis placeat, tempora ex totam, dolore sequi harum eos voluptatibus animi labore officiis minus laboriosam
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum ratione facere animi impedit rem labore sint repellendus ipsa sapiente voluptatem aut excepturi quo itaque, ab earum cumque. Voluptatem beatae id inventore quod voluptate qui deserunt, quis placeat, tempora ex totam, dolore sequi harum eos voluptatibus animi labore officiis minus laboriosam
+        </p>
+    </div>
+
+
+
+    <!-- Jquery needed -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="./partials/js/scripts.js"></script>
+
+    <!-- Function used to shrink nav bar removing paddings and adding black background -->
+    <script>
+        $(window).scroll(function() {
+            if ($(document).scrollTop() > 50) {
+                $('.nav').addClass('affix');
+                console.log("OK");
+            } else {
+                $('.nav').removeClass('affix');
+            }
+        });
+    </script>
+
+
+    <?php require "./partials/footer.php" ?>
